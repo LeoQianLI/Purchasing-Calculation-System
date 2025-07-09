b250ca305e9d11819a2c56f44e8d5d9ce3ebdef6
Léo <leo126com@gmail.com>
2025-07-09 14:50:07 +0200

    Update streamlit_reappro.py

--- streamlit_reappro.py
+++ streamlit_reappro.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
+
+# Fix matplotlib backend for Streamlit Cloud
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 import plotly.express as px
